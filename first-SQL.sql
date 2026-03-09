cb00bee80b0308119a979669a70bd7da54b0799c
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-03-08 21:50:15 -0800

    update

--- first-SQL.sql
+++ first-SQL.sql
@@ -1,10 +1,1 @@
-
-	create table employee(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(100) not NULL,
-    EMAIL varchar(100) unique NOT NULL,
-    GENDER ENUM('Male','Female','Other'),
-    date_of_birth date,
-    create_at timestamp default current_timestamp
-    
-    );
+select * from employee;
