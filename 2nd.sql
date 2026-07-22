ddd71fa24e68a5c3e4a01249183a1a410c2a280d
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-07-22 20:44:11 +0530

    update

--- 2nd.sql
+++ 2nd.sql
@@ -2,3 +2,20 @@ create DATABASE NEW_NAME;
 create database TEMP1;
 create database COLLAGE;
 DROP database TEMP1;
+drop database NEW_NAME;
+USE  COLLAGE;
+create table Student (
+id int primary key,
+name varchar(50),
+age int not null
+);
+INSERT INTO  Student values(1,"Aman",18);
+INSERT INTO  Student values(2,"Raman",28);
+INSERT INTO  Student values(3,"harman",19);
+INSERT INTO  Student values(4,"ram",10);
+INSERT INTO  Student values(5,"sham",38);
+INSERT INTO  Student values(6,"kamal",20);
+
+select * FROM Student;
+
+
