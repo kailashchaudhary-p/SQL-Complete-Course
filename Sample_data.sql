8bb71707557e89b482060f2e00873f738d391dd2
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-07-31 21:49:48 +0530

    update

--- Sample_data.sql
+++ Sample_data.sql
@@ -1,4 +1,11 @@
-CREATE DATABASE University;
-CREATE TABLE Student(
- 
- );
+CREATE DATABASE IF NOT EXISTS collage;
+
+USE collage;
+
+CREATE TABLE students (
+    rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(30)
+);
