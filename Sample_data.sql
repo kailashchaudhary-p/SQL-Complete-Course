7fef9d7fe738084526194301e4cbec9018e749b2
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-08-02 22:11:15 +0530

    update

--- Sample_data.sql
+++ Sample_data.sql
@@ -9,3 +9,20 @@ CREATE TABLE students (
     grade VARCHAR(1),
     city VARCHAR(30)
 );
+use collage;
+insert into students
+(rollno,name,marks,grade,city)
+values
+(101,"anil",78,"C","Pune"),
+(102,"Bhumika",98,"A","Mumbai"),
+(103,"Priya",91,"A","Mumbai"),
+(104,"Karan",67,"B","Jaipur"),
+(105,"Neha",74,"C","Chandigarh"),
+(106,"Aman",88,"A","Ludhiana"),
+(107,"Simran",81,"B","Amritsar"),
+(108,"Rohit",59,"D","Lucknow"),
+(109,"Pooja",95,"A","Bhopal"),
+(110,"Vikas",72,"C","Patna"),
+(1011,"Rahul",85,"A","Delhi");
+
+
